c366dceceb6319265cd8b5fcc382666804368efd
Ali Arsalan <aliarslanansari@hotmail.com>
2020-11-12 01:16:00 +0530

    alternate way to declare variables

--- cards-cmdline-project/main.go
+++ cards-cmdline-project/main.go
@@ -3,6 +3,9 @@ package main
 import "fmt"
 
 func main() {
-	var card string = "Ace of Spades"
+	// var card string = "Ace of Spades"
+	card := "Ace of Spades" //exactly same as above code
+	fmt.Println(card)
+	card = "Five of diamonds"
 	fmt.Println(card)
 }
