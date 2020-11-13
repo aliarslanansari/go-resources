5eaa25d6e06b75066fd9f8e69c6f5e10a92456f5
Ali Arsalan <aliarslanansari@hotmail.com>
2020-11-13 22:14:10 +0530

    Slices and For Loops

--- cards-cmdline-project/main.go
+++ cards-cmdline-project/main.go
@@ -3,18 +3,13 @@ package main
 import "fmt"
 
 func main() {
-	// var card string = "Ace of Spades"
-	card := "Ace of Spades" //exactly same as above code | Data type of card will be set to string as we are assigning a string to it. (:=)
-	card = newCardString()  // we can assign only string to it as during declaration we have set it to string | newCardString() func returns a string
-	newCard := newCardInt() // data type of newCard variable is automatically set to int as newCardInt() func returns int.
-	fmt.Println(card)
-	fmt.Println(newCard)
+	cards := []string{"Ace 	of Diamonds", newCard()}
+	cards = append(cards, "Six of spades")
+	for i, card := range cards {
+		fmt.Println(i, card)
+	}
 }
 
-func newCardString() string {
+func newCard() string {
 	return "Five of Diamonds"
-}
-
-func newCardInt() int {
-	return 12
 }
