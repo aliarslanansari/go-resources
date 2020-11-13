1fc8506ca16482f8706df1cac4e5f0bfd1e18ea2
Ali Arsalan <aliarslanansari@hotmail.com>
2020-11-13 20:56:09 +0530

    Functions and Return Types

--- cards-cmdline-project/main.go
+++ cards-cmdline-project/main.go
@@ -4,8 +4,17 @@ import "fmt"
 
 func main() {
 	// var card string = "Ace of Spades"
-	card := "Ace of Spades" //exactly same as above code
+	card := "Ace of Spades" //exactly same as above code | Data type of card will be set to string as we are assigning a string to it. (:=)
+	card = newCardString()  // we can assign only string to it as during declaration we have set it to string | newCardString() func returns a string
+	newCard := newCardInt() // data type of newCard variable is automatically set to int as newCardInt() func returns int.
 	fmt.Println(card)
-	card = "Five of diamonds"
-	fmt.Println(card)
+	fmt.Println(newCard)
+}
+
+func newCardString() string {
+	return "Five of Diamonds"
+}
+
+func newCardInt() int {
+	return 12
 }
